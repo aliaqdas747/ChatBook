c57e848f11de00ceadc05150cae8c3942fab6235
aliaqdas747 <142832510+aliaqdas747@users.noreply.github.com>
2024-04-24 17:04:53 +0500

    Worked On Light & Dark Theme, ==>>ChatBook

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat_app/themes/light_mode.dart';
+import 'package:flutter/material.dart';
+
+import 'Pages/login_page.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: lightMode,
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
